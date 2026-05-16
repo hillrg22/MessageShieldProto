0a470756a25eb9cf530a15aca90b2dd7ec0cdad7
Rob Hill <hillrg22@gmail.com>
2026-05-16 00:38:03 -0500

    Initial Commit

---
+++ MessageShieldProto/MessageShieldProtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageShieldProtoApp.swift
+//  MessageShieldProto
+//
+//  Created by Rob Hill on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct MessageShieldProtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
